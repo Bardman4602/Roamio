eaa6658cf3c846762ecf3c4011b333d300b9dbd6
Bardman <jonaswittstrom1@gmail.com>
2025-02-28 13:28:14 +0100

    Add project files.

---
+++ Roamio.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Roamio.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Roamio.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Roamio.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
